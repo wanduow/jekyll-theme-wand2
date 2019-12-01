8dcbe0cc75f3a066a160de4e4d8b0f83dab1a08b
Jorsin <Jorsin@github.com>
2019-12-02 09:12:47 +1300

    Created mobile version and made layouts mode modular

---
+++ assets/js/theme.js
@@ -0,0 +1,41 @@
+function clean(node)
+{
+  for(var n = 0; n < node.childNodes.length; n ++)
+  {
+    var child = node.childNodes[n];
+    if
+    (
+      child.nodeType === 8 
+      || 
+      (child.nodeType === 3 && !/\S/.test(child.nodeValue))
+    )
+    {
+      node.removeChild(child);
+      n --;
+    }
+    else if(child.nodeType === 1)
+    {
+      clean(child);
+    }
+  }
+}
+
+function toggleMenu() {
+    var extendedItems = document.getElementsByClassName("extended");
+    var nav = document.getElementsByTagName("nav");
+
+    if(extendedItems.length > 0){
+        var navItems = document.getElementsByClassName("navlink");
+        nav[0].classList.remove("extendnav");
+        for (i = 0; i < navItems.length; i++){
+            navItems[i].classList.remove("extended");
+        }
+    }
+    else {
+        var navItems = document.getElementsByClassName("navlink");
+        nav[0].classList.add("extendnav");
+        for (i = 0; i < navItems.length; i++){
+            navItems[i].classList.add("extended");
+        }
+    }
+}

d1976bd26ad8f38d0eb0822e86c066931ccfcb58
Jorsin <Jorsin@users.noreply.github.com>
2019-12-15 20:23:14 -0600

    Fixed nav misalignment caused by scroll bar

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -34,3 +34,46 @@ function toggleMenu() {
         }
     }
 }
+
+function checkScrollBars() {
+  var clientHeight = document.getElementById('container').clientHeight;
+
+  //console.log('Scroll height: ' + clientHeight + ' height: ' + document.body.clientHeight + ' width: ' + window.innerWidth);
+  if(clientHeight > document.body.clientHeight && window.innerWidth > 750){
+      document.getElementById('body').style.marginRight = '-' + getScrollBarWidth() + 'px';
+  }
+  else {
+    document.getElementById('body').style.marginRight = '0';
+  }
+}
+
+function getScrollBarWidth () {
+  var inner = document.createElement('p');
+  inner.style.width = "100%";
+  inner.style.height = "200px";
+
+  var outer = document.createElement('div');
+  outer.style.position = "absolute";
+  outer.style.top = "0px";
+  outer.style.left = "0px";
+  outer.style.visibility = "hidden";
+  outer.style.width = "200px";
+  outer.style.height = "150px";
+  outer.style.overflow = "hidden";
+  outer.appendChild (inner);
+
+  document.body.appendChild (outer);
+  var w1 = inner.offsetWidth;
+  outer.style.overflow = 'scroll';
+  var w2 = inner.offsetWidth;
+  if (w1 == w2) w2 = outer.clientWidth;
+
+  document.body.removeChild (outer);
+
+  return (w1 - w2);
+}
+
+window.addEventListener('resize', function(event){
+  console.log('Testing 123');
+  checkScrollBars();
+});
